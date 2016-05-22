cdb8ccf1907a1502979133372dc1c0f922c2bb2f
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-05-22 22:54:22 +0300

    Automount ntfs volume

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Skype & Dropbox
+#
+
 wget -O skype.deb https://get.skype.com/go/getskype-linux-ubuntu-64
 wget -O dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
 
@@ -12,12 +16,18 @@ sudo apt-get install -f
 
 rm skype.deb dropbox.deb
 
+#
+# Git
+#
 
 sudo apt-get install git gitk
 
 git config --global user.email "oleh.palianytsia@gmail.com"
 git config --global user.name "Oleh Palianytsia"
 
+#
+# Emacs
+#
 
 mkdir -p ~/devel
 cd ~/devel
@@ -35,3 +45,11 @@ sudo aptitude build-dep emacs24
 make -j8
 sudo make install
 cd
+
+
+#
+# Fstab config
+#
+
+sudo mkdir -p /media/files
+sudo echo "/dev/sda3   /media/files  ntfs    defaults    0   0" >> /etc/fstab

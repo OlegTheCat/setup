802706f0529b0fee14f5bdd481a4a4b409d1dea0
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-05-22 22:33:28 +0300

    Automate dropbox & skype installation

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -O skype.deb https://get.skype.com/go/getskype-linux-ubuntu-64
+wget -O dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+
+sudo apt-get update
+
+sudo dpkg -i skype.deb
+sudo dpkg -i dropbox.deb
+
+sudo apt-get install -f
+
+rm skype.deb dropbox.deb
